bcf9fb2f8103466f5db254df50dc0855d94ec0e1
mark818 <krbharathala@berkeley.edu>
2017-10-30 19:09:40 -0700

    add firingcheck and firing

---
+++ Assets/ArrowsScripts/CheckFiring.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public OVRInput.Controller controllerLeft;
+    public OVRInput.Controller controllerRight;
+
+    private float [] indexTriggerState = { 0, 0 };
+    private float [] handTriggerState = { 0, 0 };
+    private float [] oldIndexTriggerState = { 0, 0 };
+
+    
+
+    // Use this for initialization
+    void Start () {
+		
+        
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        updateTriggers();
+
+        checkFiring();
+
+
+
+    }
+
+    void updateTriggers()
+    {
+        oldIndexTriggerState[0] = indexTriggerState[0];
+        indexTriggerState[0] = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controllerLeft);
+        handTriggerState[0] = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controllerLeft);
+
+        oldIndexTriggerState[1] = indexTriggerState[1];
+        indexTriggerState[1] = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controllerRight);
+        handTriggerState[1] = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controllerRight);
+    }
+
+    void checkFiring()
+    {
+        if (indexTriggerState[0] > 0.9f && oldIndexTriggerState[0] < 0.9f)
+        {
+            //Insert Firing Here
+        }
+
+        if (indexTriggerState[1] > 0.9f && oldIndexTriggerState[1] < 0.9f)
+        {
+            //Insert Firing Here
+        }
+
+
+
+    }
+}
---
+++ Assets/ArrowsScripts/Firing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour {
+
+    public enum hands {Left, Right};
+    public OVRInput.Controller controllerLeft;
+    public OVRInput.Controller controllerRight;
+
+    public float firingSpeed;
+    public GameObject bulletType;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Fire(hands leftOrRight)
+    {
+        if (leftOrRight == hands.Left)
+        {
+            Vector3 LeftLocation = OVRInput.GetLocalControllerPosition(controllerLeft);
+            Quaternion LeftRotation = OVRInput.GetLocalControllerRotation(controllerRight);
+
+
+        }
+
+        if (leftOrRight == hands.Right)
+        {
+            Vector3 RightLocation = OVRInput.GetLocalControllerPosition(controllerRight);
+            Quaternion RightRotation = OVRInput.GetLocalControllerRotation(controllerRight);
+
+        }
+    }
+}
