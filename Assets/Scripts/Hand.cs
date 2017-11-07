be62202526f972c13a841eaabad2154e83a8e5f7
krbharathala <krbharathala@berkeley.edu>
2017-11-06 18:53:18 -0800

    Created the spawning blocks script and the hand script

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour {
+
+    public OVRInput.Controller controller;
+    private float indexTriggerState = 0;
+    private float handTriggerState = 0;
+    private float oldIndexTriggerState = 0;
+    private bool holdingBlock = false;
+    private GameObject block = null;
+    public Vector3 holdPosition = new Vector3(0, -0.025f, 0.03f);
+    public Vector3 holdRotation = new Vector3(0, 180, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        oldIndexTriggerState = indexTriggerState;
+        indexTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller);
+        handTriggerState = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller);
+        if (holdingBlock) {
+            if (handTriggerState < 0.9f) {
+                Release();
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("a")) {
+            if (handTriggerState > 0.9f && !holdingBlock) {
+                Grab(other.gameObject);
+            }
+        }
+    }
+
+    void Grab(GameObject obj)
+    {
+        holdingBlock = true;
+        block = obj;
+        block.transform.parent = transform;
+        block.transform.localPosition = holdPosition;
+        block.transform.localEulerAngles = holdRotation;
+        block.GetComponent<Rigidbody>().useGravity = false;
+        block.GetComponent<Rigidbody>().isKinematic = true;
+
+
+    }
+
+    void Release() {
+        block.transform.parent = null;
+        Rigidbody rigidbody = block.GetComponent<Rigidbody>();
+        rigidbody.useGravity = true;
+        rigidbody.isKinematic = false;
+        rigidbody.velocity = OVRInput.GetLocalControllerVelocity(controller);
+        holdingBlock = false;
+        block = null;
+
+    }
+}
--- Assets/Scripts/SpawnBlock.cs
+++ Assets/Scripts/SpawnBlock.cs
@@ -4,13 +4,44 @@ using UnityEngine;
 
 public class SpawnBlock : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public GameObject a;
+	public GameObject b;
+	public GameObject c;
+	public GameObject d;
+	public GameObject e;
+	public GameObject f;
+	public GameObject g;
+	public GameObject h;
+    GameObject[] blocks = new GameObject[8];
+    float next_spawn_time = Time.time+5.0f;
+    //Physics.gravity = new Vector3(0, -1.0F, 0);
+
+
+
+    // Use this for initialization
+    void Start () {
+        blocks[0] = a;
+        blocks[1] = b;
+        blocks[2] = c;
+        blocks[3] = d;
+        blocks[4] = e;
+        blocks[5] = f;
+        blocks[6] = g;
+        blocks[7] = h;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Time.time > next_spawn_time) {
+            int block_num = Random.Range(0, 8);
+            float x_num = Random.Range(0.0f, 1.0f);
+            float z_num = Random.Range(0.0f, 1.0f);
+            GameObject clone = GameObject.Instantiate(blocks[block_num], new Vector3(x_num, 10, z_num + 5), Quaternion.identity);
+            clone.AddComponent<Rigidbody>();
+            clone.AddComponent<BoxCollider>();
+            //clone.GetComponent<Rigidbody>().AddForce(new Vector3(0, 5f, 0));
+            next_spawn_time += 5.0f;
+        }
+
+    }
 }
