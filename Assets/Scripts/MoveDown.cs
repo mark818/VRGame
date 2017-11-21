d25440d0af47b3f3301c28c2298db5ab5d032bed
Austin <davisaustin@berkeley.edu>
2017-11-20 19:05:49 -0800

    almost working moving down

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveDown : MonoBehaviour {
+    public bool isMoving = true;
+    private float time;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        time = Time.time;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        
+        if (Time.time >= time + 1f && isMoving == true)
+        {
+            this.transform.Translate(0f, -.1f, 0f);
+            time = Time.time;
+        }
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        isMoving = false;
+    }
 }
--- Assets/Scripts/SpawnBlock.cs
+++ Assets/Scripts/SpawnBlock.cs
@@ -14,6 +14,8 @@ public class SpawnBlock : MonoBehaviour {
 	public GameObject h;
     GameObject[] blocks = new GameObject[8];
     float next_spawn_time = Time.time+5.0f;
+    public MoveDown previous;
+    //public MoveDown md; 
     //private Vector3 
     //Physics.gravity = new Vector3(0, -1.0F, 0);
 
@@ -29,23 +31,23 @@ public class SpawnBlock : MonoBehaviour {
         blocks[5] = f;
         blocks[6] = g;
         blocks[7] = h;
+        previous = null;
     }
 
     // Update is called once per frame
     void Update () {
-        if (Time.time > next_spawn_time) {
+        if (Time.time > next_spawn_time && (previous == null || previous.isMoving == false )) {
             int block_num = Random.Range(0, 8);
             GameObject clone = GameObject.Instantiate(blocks[block_num], new Vector3(.722f, 3.1f, 5.317f), Quaternion.identity);
             clone.AddComponent<Rigidbody>();
             clone.GetComponent<Rigidbody>().useGravity = false;
-
-
-            clone.AddComponent(MoveDown);
+            clone.AddComponent<MoveDown>();
+            previous = clone.GetComponent<MoveDown>();
 
 
             //clone.GetComponent<Rigidbody>().velocity = new Vector3(0, -1.0f, 0);
             //clone.GetComponent<Rigidbody>().AddForce(new Vector3(0, 5f, 0));
-            //next_spawn_time += 5.0f;
+            next_spawn_time += 5.0f;
             //Physics.gravity = new Vector3(0, -0.5f, 0);
 
             
